e8c5d9b59e6737e1c1e3d2f4ba07866fd6f7f44a
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-01-05 09:48:04 +0600

    project completed

--- db/pool.js
+++ db/pool.js
@@ -6,5 +6,9 @@ module.exports = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
